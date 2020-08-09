497f0eba7c5e2f631e7905d6070862ff2b0f185b
HansAchinike <68516824+HansAchinike@users.noreply.github.com>
2020-08-09 16:59:54 +0100

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+	<h1>WELCOME</h1>
+	<h2>These Are Your Details</h2>
+	<h3><?php echo $_POST["Name"];?>
+    <br>
+ <h3>Your Email Address is: <?php echo $_POST["Email"];
+ 
+
+?>					
+
+</body>
+</html>
