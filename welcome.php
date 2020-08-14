963876e46f5ebbcac6c63773dce50c77d23e57dd
Johnnyevans32 <evanseburu32@gmail.com>
2020-08-15 00:26:00 +0100

    corrections

--- welcome.php
+++ welcome.php
@@ -1,12 +1,17 @@
+<?php
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html>
 <body style = " background-color : <?php echo  $_SESSION['color']; ?> ">
 
  <?php
-    echo '<h3 align ="center"><i> You are welcome '. $_POST['name'].' '. $_POST['lname']. ',</i></h3>';
-    echo '<h3 align ="center"><i> You have officially Registered as a member of the ' .$_POST["dept"].' department</i></h3>';
-    echo '<h3 align ="center"><i> Your email address is '. $_POST['email'].'</i></h3>';
-    echo '<h3 align ="center"><i> Your Date of birth is '. $_POST['dob'].'.</i></h3>';
+    echo '<h3 align ="center"><i> You are welcome '. $_SESSION['name'].' '. $_SESSION['lname']. ',</i></h3>';
+    echo '<h3 align ="center"><i> You have officially Registered as a member of the ' .$_SESSION["dept"].' department</i></h3>';
+    echo '<h3 align ="center"><i> Your email address is '. $_SESSION['email'].'</i></h3>';
+    echo '<h3 align ="center"><i> Your Date of birth is '. $_SESSION['dob'].'.</i></h3>';
     echo '<h1 align ="center"><strong> Explore <strong></h1>';
 	
 ?>					
