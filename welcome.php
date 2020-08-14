4c1f5375525e815561e65e185f274c68106b754f
HansAchinike <68516824+HansAchinike@users.noreply.github.com>
2020-08-14 19:31:19 +0100

    Add files via upload

--- welcome.php
+++ welcome.php
@@ -1,23 +1,19 @@
-<?php 
-	session_start();
-	if (isset($_SESSION['color']) AND isset($_SESSION['email']) AND isset($_SESSION['name']) ) {
-		$color = $_SESSION['color'];
-		$email = $_SESSION['email'];
-		$name = $_SESSION['name'];
-	}
-	else{
-		echo "<script>alert ('Please Login or Register!!')</script>";
-		echo "<script>window.open('wejapa wk3.php','_self')</script>";
-	}
-?>
 <!DOCTYPE html>
 <html>
-<body style="background-color: <?php echo $color;?>;">
+<body style = " background-color : <?php echo  $_SESSION['color']; ?> ">
 
-	<h1>WELCOME</h1>
-	<h2>These Are Your Details</h2>
-	<h3><?php echo $name;?></h3>
-    <br>
-	<h3>Your Email Address is: <?php echo $email;?></h3>					
+ <?php
+    echo '<h3 align ="center"><i> You are welcome '. $_POST['name'].' '. $_POST['lname']. ',</i></h3>';
+    echo '<h3 align ="center"><i> You have officially Registered as a member of the ' .$_POST["dept"].' department</i></h3>';
+    echo '<h3 align ="center"><i> Your email address is '. $_POST['email'].'</i></h3>';
+    echo '<h3 align ="center"><i> Your Date of birth is '. $_POST['dob'].'.</i></h3>';
+    echo '<h1 align ="center"><strong> Explore <strong></h1>';
+	
+?>					
+
 </body>
 </html>
+
+
+
+
